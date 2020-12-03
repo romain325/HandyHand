a5d02e614c6a020ac7224f71a723ae0ed40531a9
romain325 <romainolivier42@gmail.com>
2020-12-03 17:07:48 +0100

    React

--- HandyHandFront/app/components/Home.tsx
+++ HandyHandFront/app/components/Home.tsx
@@ -8,6 +8,7 @@ export default function Home(): JSX.Element {
     <div className={styles.container} data-tid="container">
       <h2>Home</h2>
       <Link to={routes.COUNTER}>to Counter</Link>
+      <Link to={routes.CONNEXION}>to Connexion</Link>
     </div>
   );
 }
