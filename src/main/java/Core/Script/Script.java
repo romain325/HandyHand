62750c5ece60a553db7e9a605c3860acad39b6f1
romain325 <romainolivier42@gmail.com>
2020-12-09 13:38:35 +0100

    add script launch

--- src/main/java/Core/Script/Script.java
+++ src/main/java/Core/Script/Script.java
@@ -1,33 +1,57 @@
 package Core.Script;
 
-public class Script implements Runnable{
-    private static String userOS;
-    private final String path;
-    private final String[] args;
-    private final String type;
+import processing.data.JSONObject;
 
-    public String getPath() {
-        return path;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.prefs.Preferences;
+
+public class Script implements Runnable {
+    private static final int EXEC_TIME_COOLDOWN = 3000;
+    private static String userOS = System.getProperty("os.name");
+    private final String execPath;
+    private final String[] args;
+    private final String file;
+
+    public String getExecPath() {
+        return execPath;
     }
 
     public String[] getArgs() {
         return args;
     }
 
-    public String getType() {
-        return type;
+    public String getFile() {
+        return file;
     }
 
-    public Script(String path, String[] args, String type) {
-        this.path = path;
+    public Script(String execPath, String[] args, String file) {
+        this.execPath = execPath;
         this.args = args;
-        this.type = type;
+        this.file = file;
     }
 
     @Override
     public void run() {
-        // Do some shit
-        // TODO see with @yoperiquoi for scripts
-        System.out.println("Finally");
+        try {
+            // if you want to have access to a python script on Windows you have to specify the Path to your python .exe
+            Process process = new ProcessBuilder(this.execPath,this.file).start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+            while ( (line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.getProperty("line.separator"));
+            }
+            System.out.println(builder.toString());
+
+            Thread.sleep(EXEC_TIME_COOLDOWN);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            // TODO execption ?
+        }
     }
+
+
 }
