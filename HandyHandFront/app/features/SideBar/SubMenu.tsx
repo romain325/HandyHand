ec2bbba28ee2336378ea1b44415f30d4cce814cf
romain325 <romainolivier42@gmail.com>
2020-12-10 10:01:04 +0100

    [DEV] Add SideBar (no styling)

---
+++ HandyHandFront/app/features/SideBar/SubMenu.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Nav } from 'react-bootstrap';
+import classNames from 'classnames';
+import { Accordion } from 'react-bootstrap';
+
+type SubItem = {
+  link: string,
+  title: string
+}
+
+type SubMenuElements = {
+  title: string,
+  items: Array<SubItem>
+}
+
+
+
+const SubMenu = ({title, items} : SubMenuElements) => {
+  const [isCollapsed, setCollapsed] = useState<boolean>(true);
+
+  function toggleNav(){
+    setCollapsed(!isCollapsed);
+  }
+
+  return (
+    <Nav.Item className={classNames({open: !isCollapsed})}>
+      <Accordion>
+        <Accordion.Toggle
+          as={Nav.Link}
+          variant="link"
+          eventKey="0"
+          onClick={toggleNav}
+        >
+          {title}
+        </Accordion.Toggle>
+
+        <Accordion.Collapse eventKey="0">
+          <nav className="nav flex-column">
+            {
+              items.map(item => (
+                <a className="nav-link nav-item pl-5"
+                  href={item.link}
+                  key={item.title}>
+                    {item.title}
+                </a>
+              ))
+            }
+          </nav>
+        </Accordion.Collapse>
+
+      </Accordion>
+    </Nav.Item>
+  )
+}
+
+export default SubMenu;
--- HandyHandFront/app/features/counter/Counter.tsx
+++ HandyHandFront/app/features/counter/Counter.tsx
@@ -1,56 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Counter.css';
 import routes from '../../constants/routes.json';
 import { Col, Container } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import SideBar from '../SideBar/SideBar';
+import { Button } from 'react-bootstrap';
 
 
 export default function Counter() {
+  const [isOpen, setOpen] = useState<boolean>(false);
+
+  function toggleNavBar(){
+    setOpen(!isOpen);
+    console.log(isOpen);
+  }
+
   return (
-    <div>
+    <Container>
+      <Row>
+        <Col>
+          <SideBar isOpen={isOpen} toggleBar={toggleNavBar} />
+        </Col>
 
-      <div className={styles.backButton} data-tid="backButton">
-        <Link to={routes.HOME}>
-          <i className="fa fa-arrow-left fa-3x" />
-        </Link>
-      </div>
+        <Col>
+          <div className={styles.backButton} data-tid="backButton">
+            <Link to={routes.HOME}>
+              <i className="fa fa-arrow-left fa-3x" />
+            </Link>
+          </div>
 
-      <Container>
-        <Row>
-          <Col>
-            <ButtonGroup vertical>
+          <Button onClick={toggleNavBar} >YOUHOU</Button>
 
-            </ButtonGroup>
-          </Col>
-          <Col xs={6}>2 of 3 (wider)</Col>
-          <Col>3 of 3</Col>
-        </Row>
-      </Container>
+          <Container>
+            <Row>
+              <Col>
+                <ButtonGroup vertical>
 
-      <Container className={styles.Contain}>
-        <Row className="border border-dark ">
-          <Col className="border border-dark ">Nom</Col>
-          <Col className="border border-dark ">Description</Col>
-          <Col className="border border-dark ">Date de denrière édition</Col>
-        </Row>
-        <Row className="border border-dark ">
-          <Col>Nom</Col>
-          <Col>Description</Col>
-          <Col>Date de denrière édition</Col>
-        </Row>
-        <Row className="border border-dark ">
-          <Col>Nom</Col>
-          <Col>Description</Col>
-          <Col>Date de denrière édition</Col>
-        </Row>
-        <Row className="border border-dark ">
-          <Col>Nom</Col>
-          <Col>Description</Col>
-          <Col>Date de denrière édition</Col>
-        </Row>
-      </Container>
-    </div>
+                </ButtonGroup>
+              </Col>
+              <Col xs={6}>2 of 3 (wider)</Col>
+              <Col>3 of 3</Col>
+            </Row>
+          </Container>
+
+          <Container className={styles.Contain}>
+            <Row className="border border-dark ">
+              <Col className="border border-dark ">Nom</Col>
+              <Col className="border border-dark ">Description</Col>
+              <Col className="border border-dark ">Date de denrière édition</Col>
+            </Row>
+            <Row className="border border-dark ">
+              <Col>Nom</Col>
+              <Col>Description</Col>
+              <Col>Date de denrière édition</Col>
+            </Row>
+            <Row className="border border-dark ">
+              <Col>Nom</Col>
+              <Col>Description</Col>
+              <Col>Date de denrière édition</Col>
+            </Row>
+            <Row className="border border-dark ">
+              <Col>Nom</Col>
+              <Col>Description</Col>
+              <Col>Date de denrière édition</Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 }
