bac779e5e6d089075cc2a2b7f5b7f772f340a650
romain325 <romainolivier42@gmail.com>
2021-01-07 18:24:20 +0100

    Add POST request for Script

--- src/main/java/Api/ApiApp/ScriptRESTController.java
+++ src/main/java/Api/ApiApp/ScriptRESTController.java
@@ -2,11 +2,17 @@ package Api.ApiApp;
 
 import Core.Script.Script;
 import Core.StubPersistence.ScriptPersistance;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import org.json.simple.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import javax.naming.NameNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/script")
@@ -39,6 +45,31 @@ public class ScriptRESTController {
         } catch (NameNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    @PostMapping("/add")
+    public boolean addScript(HttpServletRequest req, @RequestBody String data) {
+        //TODO Verify Identity
+
+        var obj = new Gson().fromJson(data, JsonObject.class);
+        List<String> args = new ArrayList<>();
+        for (var elem : obj.getAsJsonArray("args")){
+            args.add(elem.getAsString());
+        }
+        String file = obj.get("file").getAsString();
+        String execPath = obj.get("execPath").getAsString();
+
+        if(file.isEmpty() || file.isBlank() || execPath.isEmpty() || execPath.isBlank()) return false;
+
+        Script script = new Script(execPath, args.toArray(new String[0]), file);
+        try {
+            new ScriptPersistance().getById(script.getId());
+        } catch (NameNotFoundException e) {
+            new ScriptPersistance().save(script);
+            return true;
+        }
+
+        return false;
     }
 
 }
