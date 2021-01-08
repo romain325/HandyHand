08e7ab9fbad7a10cef35f67f24a3e5b2ff8d7a9e
romain325 <romainolivier42@gmail.com>
2021-01-08 18:14:41 +0100

    GET image from API

--- src/main/java/Api/ApiApp/LeapInfosRESTController.java
+++ src/main/java/Api/ApiApp/LeapInfosRESTController.java
@@ -2,24 +2,40 @@ package Api.ApiApp;
 
 import Visual.ProcessingVisual.CameraView.CameraImage;
 import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Leap;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.print.attribute.standard.Media;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Base64;
 
 @RestController
 @RequestMapping("/leap")
 public class LeapInfosRESTController {
-    private Controller controller = new Controller();
 
     @GetMapping("/state")
     public boolean getState(){
-        return controller.isConnected();
+        return new Controller().isConnected();
     }
 
-    @GetMapping("/view")
-    public String getLeapView() {
-        return Base64.getEncoder().encodeToString(new CameraImage().getByteArrayImage());
+    @GetMapping(value="/view")
+    public void getLeapView(HttpServletResponse response) throws IOException, URISyntaxException {
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+
+        byte[] img = new CameraImage().getByteArrayImage();
+
+        IOUtils.copy((img == null || img.length == 0) ? new ClassPathResource("img/defaultImage.jpg").getInputStream() : new ByteArrayInputStream(img), response.getOutputStream());
     }
 }
