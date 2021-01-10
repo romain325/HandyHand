4eeb2042f04160f84c6b37152c4ee4f35e97cd96
romain325 <romainolivier42@gmail.com>
2021-01-11 00:12:11 +0100

    Add Leap Render on React

--- HandyHandFront/app/features/accueil/AccueilFeature.tsx
+++ HandyHandFront/app/features/accueil/AccueilFeature.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
 import ContentPage from "../../containers/ContentPage";
 
 
+
+
 export default function AccueilFeature() {
+
+  const [seconds, setSeconds] = useState(0);
+
+  function getCurrentImage() : JSX.Element {
+    return <img src={`http://localhost:8080/leap/view?rdm=${seconds}`} alt={`Img ${seconds}`} height={100} width={100}/>;
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log(seconds);
+      setSeconds(seconds => seconds+1);
+    }, 500);
+    return () => clearInterval(interval);
+  }, [seconds]);
+
   return (
     <ContentPage>
-      <Form>
-        <Form.Check
-          type="switch"
-          id="custom-switch"
-          label="actived"
-        />
-      </Form>
+      <div>
+        <Form>
+          <Form.Check
+            type="switch"
+            id="custom-switch"
+            label="actived"
+          />
+        </Form>
+        { getCurrentImage() }
+      </div>
     </ContentPage>
   );
  }
