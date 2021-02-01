6033cff5be45068027498179315b0729c404e173
Yoann PERIQUOI <yoann.periquoi@etu.uca.fr>
2021-02-01 10:20:07 +0100

    commit for romain to get my code

---
+++ src/main/java/DataBase/Repositories/ScriptRepository.java
@@ -0,0 +1,8 @@
+package DataBase.Repositories;
+
+import Core.Script.Script;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public class ScriptRepository implements MongoRepository<Script, String> {
+
+}
