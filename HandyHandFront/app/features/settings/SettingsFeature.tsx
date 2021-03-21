c7f2a4f9568969cf0500a36bcce38d1a42fd5457
romain325 <romainolivier42@gmail.com>
2021-03-22 00:11:09 +0100

    Address update from React

--- HandyHandFront/app/features/settings/SettingsFeature.tsx
+++ HandyHandFront/app/features/settings/SettingsFeature.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import { Card, Col, Form, Row } from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 import ContentPage from '../../containers/ContentPage';
+import {
+  getAddress,
+  setAddress,
+} from '../../utils/HandyHandAPI/HandyHandConfig';
+
+
 
 export default function SettingsFeature() {
+  const [addr, setAddr] = useState<string>(getAddress());
+  const onAddressChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setAddress(e.currentTarget.value);
+    setAddr(e.currentTarget.value);
+  };
+
   return (
     <ContentPage childrenName="Settings">
       <Row>
@@ -16,14 +28,8 @@ export default function SettingsFeature() {
               <h1>Settings</h1>
             </Card.Title>
             <Card.Body>
-              <h3>Chemin d'accès a l'éxécuteur</h3>
-              <Form.File
-                className="position-relative"
-                required
-                name="file"
-                id="validationFormik107"
-                feedbackTooltip
-              />
+              <h3>API REST Address</h3>
+              <input type="text" value={addr} onChange={onAddressChange} />
             </Card.Body>
           </Card>
         </Col>
--- HandyHandFront/app/utils/HandyHandAPI/HandyHandConfig.ts
+++ HandyHandFront/app/utils/HandyHandAPI/HandyHandConfig.ts
@@ -3,8 +3,12 @@ import fs from 'fs';
 const address = 'api.address';
 
 export function getAddress(): string {
-  if(!fs.existsSync(address)){
+  if (!fs.existsSync(address)) {
     fs.writeFileSync(address, 'http://localhost:8080', { encoding: 'utf8' });
   }
   return fs.readFileSync(address, 'utf8');
+}
+
+export function setAddress(newAddress: string): void {
+  fs.writeFileSync(address, newAddress, { encoding: 'utf8' });
 }
